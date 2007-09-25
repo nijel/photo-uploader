42e1679586cc98c1bcb128d54b843bfae97091a6
Michal Čihař <michal@cihar.com>
2007-09-25 05:27:22 +0000

    Convert command line to accept options and use new module.

--- do-upload.py
+++ do-upload.py
@@ -24,97 +24,46 @@ this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 '''
 
-import pycurl
-import StringIO
-import os
-import sys
+from optparse import OptionParser
+import phoupl
 import webbrowser
+import sys
 
-debug = False
+# Parameters processing
+usage = "usage: %prog [options] images"
+parser = OptionParser(usage=usage)
+parser.add_option("-s", "--service",
+                  action="store", type="string",
+                  dest="service_name", default="droxi.cz",
+                  help="Name of service to use.")
+parser.add_option("-l", "--list-services",
+                  action="store_true",
+                  dest="list_services", default=False,
+                  help="List available services.")
+parser.add_option("-b", "--open-browser",
+                  action="store_true",
+                  dest="open_browser", default=False,
+                  help="Open order in browser after uploading.")
+parser.add_option("-d", "--debug",
+                  action="store_true",
+                  dest="debug", default=False,
+                  help="Show debugging output.")
 
-def dumpRequest(c, b):
-    if debug:
-        sys.stderr.write(80 * '-')
-        sys.stderr.write('\n%d: %s\n' % (
-            c.getinfo(pycurl.HTTP_CODE), 
-            c.getinfo(pycurl.EFFECTIVE_URL)
-            ))
-        sys.stderr.write(b.getvalue())
-        sys.stderr.write('\n')
+(options, args) = parser.parse_args()
 
-def msg(s, noeol = False):
-    if noeol:
-        print s,
-    else:
-        print s
-    sys.stdout.flush()
+# List services
+if options.list_services:
+    print 'Available services:'
+    for x in phoupl.list_services():
+        print ' %s' % x
+    sys.exit(0)
 
+# Check for some files to upload
+if len(args) == 0:
+    parser.print_help()
+    sys.exit("No files to upload.")
 
-c = pycurl.Curl()
-b = StringIO.StringIO()
-# Write output to string buffer
-c.setopt(pycurl.WRITEFUNCTION, b.write)
-# We need to follow redirects
-c.setopt(pycurl.FOLLOWLOCATION, 1)
-# We need cookies support, but we don't have to store them
-c.setopt(pycurl.COOKIEFILE, "")
+url = phoupl.upload_photos(options.service_name, args, debug = options.debug)
 
-# Init session...
-msg('Initialising session...')
-c.setopt(pycurl.URL, 'http://foto.droxi.cz/')
-c.perform()
-dumpRequest(c, b)
-b.truncate(0)
-
-cookies = c.getinfo(pycurl.INFO_COOKIELIST)
-sessionid = cookies[0].split('\t')[6]
-
-# Select photo and not photoalbum...
-msg('Selecting photo order...')
-c.setopt(pycurl.URL, 'http://foto.droxi.cz/index.php')
-c.setopt(pycurl.HTTPPOST, [
-    ('scripttest', '0'), 
-    ('vyber', 'fotka')
-    ])
-c.perform()
-dumpRequest(c, b)
-b.truncate(0)
-
-# Select fast/cheaper/simple order...
-msg('Selecting fast/cheaper/simple order...')
-c.setopt(pycurl.URL, 'http://foto.droxi.cz/typ-zakazky/index.php')
-c.setopt(pycurl.HTTPPOST, [
-    ('krok_zpet', '4'), 
-    ('set_ordertype', 'disc'), 
-    ('pokracovat', 'some text')
-    ])
-c.perform()
-dumpRequest(c, b)
-b.truncate(0)
-
-# Now we have a upload form
-file_list = sys.argv[1:]
-files = len(file_list)
-for i in range(files):
-    current = file_list[i]
-    base = os.path.basename(current)
-    msg('Uploading %.01f%% [%d/%d]: %20s\r' % (100.0 * (i + 1) / files, i + 1, files, base), True)
-    sys.stdout.flush()
-    c.setopt(pycurl.HTTPPOST, [
-        ('sessionid',  sessionid), 
-        ('f1', (pycurl.FORM_FILE, current)), 
-        ('nf1', ''),
-        ('pokracovat', 'some text')
-        ])
-    c.setopt(pycurl.URL, 'http://foto.droxi.cz/vlozit-fotografie/upload.php?ilikephoto=%s&verze=droxi' % sessionid)
-    c.perform()
-    dumpRequest(c, b)
-    b.truncate(0)
-
-msg('Uploaded all %d photos                                  ' % files)
-msg('You can review them here:')
-msg('http://foto.droxi.cz/prehled-vlozenych-fotografii/?ilikephoto=%s' % sessionid)
-msg('You can finish order here:')
-msg('http://foto.droxi.cz/format-fotografii/?ilikephoto=%s' % sessionid)
-msg('Opening review URL in browser...')
-webbrowser.open('http://foto.droxi.cz/prehled-vlozenych-fotografii/?ilikephoto=%s' % sessionid)
+if options.open_browser:
+    webbrowser.open(url)

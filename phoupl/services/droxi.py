7754bef18ebc83ba177f39515d558160e4936914
Michal Čihař <michal@cihar.com>
2007-09-25 05:46:05 +0000

    No need for extra session.

--- phoupl/services/droxi.py
+++ phoupl/services/droxi.py
@@ -29,16 +29,15 @@ class DroxiService(phoupl.core.PhotoUploader):
     def _connect(self):
         # Use preseeded session
         if self._session is not None:
-            self.sessionid = self._session
-            self.msg('Reusing session %s' % self.sessionid)
+            self.msg('Reusing session %s' % self._session)
             return
 
         # Init session...
         self.msg('Initialising session...')
         self.get('http://foto.droxi.cz/')
         self.cookies = self._curl.getinfo(pycurl.INFO_COOKIELIST)
-        self.sessionid = self.cookies[0].split('\t')[6]
-        self.msg('Created session %s' % self.sessionid)
+        self._session = self.cookies[0].split('\t')[6]
+        self.msg('Created session %s' % self._session)
 
         # Select photo and not photoalbum...
         self.msg('Selecting photo order...')
@@ -59,9 +58,9 @@ class DroxiService(phoupl.core.PhotoUploader):
 
     def _upload(self, image):
         self.post(
-                'http://foto.droxi.cz/vlozit-fotografie/upload.php?ilikephoto=%s&verze=droxi' % self.sessionid,
+                'http://foto.droxi.cz/vlozit-fotografie/upload.php?ilikephoto=%s&verze=droxi' % self._session,
                 [
-                    ('sessionid',  self.sessionid), 
+                    ('sessionid',  self._session), 
                     ('f1', (pycurl.FORM_FILE, image)), 
                     ('nf1', ''),
                     ('pokracovat', 'some text')
@@ -73,9 +72,9 @@ You can review them here:
 http://foto.droxi.cz/prehled-vlozenych-fotografii/?ilikephoto=%s
 You can finish order here:
 http://foto.droxi.cz/format-fotografii/?ilikephoto=%s
-''' %(self.sessionid, self.sessionid))
+''' %(self._session, self._session))
 
     def get_review_url(self):
-        return 'http://foto.droxi.cz/prehled-vlozenych-fotografii/?ilikephoto=%s' % self.sessionid
+        return 'http://foto.droxi.cz/prehled-vlozenych-fotografii/?ilikephoto=%s' % self._session
 
 phoupl.register_service('droxi.cz', DroxiService)

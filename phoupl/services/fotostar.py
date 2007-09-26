5eb702857761ebb7d722fb1b1d62a8742b55344c
Michal Čihař <michal@cihar.com>
2007-09-26 05:48:19 +0000

    Fix description.

--- phoupl/services/fotostar.py
+++ phoupl/services/fotostar.py
@@ -1,7 +1,7 @@
 # -*- coding: UTF-8 -*-
 # vim: expandtab sw=4 ts=4 sts=4:
 '''
-Photo-Uploader wrapper for Droxi service.
+Photo-Uploader wrapper for FotoStar service.
 '''
 __author__ = 'Michal Čihař'
 __email__ = 'michal@cihar.com'

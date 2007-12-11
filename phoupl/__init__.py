739b963bc8d20cfc0b229878b7e2904a3bf6e38c
Michal Čihař <michal@cihar.com>
2007-12-11 03:50:29 +0000

    Increase version.

--- phoupl/__init__.py
+++ phoupl/__init__.py
@@ -22,7 +22,7 @@ this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 '''
 
-__version__ = '0.5'
+__version__ = '0.6'
 
 # Import main module
 import core

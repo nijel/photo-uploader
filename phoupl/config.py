bbc9cb37d3c47db67b01d245b847789396b11c81
Michal Čihař <michal@cihar.com>
2009-05-11 12:34:32 +0000

    Update copyright messages.

--- phoupl/config.py
+++ phoupl/config.py
@@ -6,7 +6,7 @@ Photo-Uploader configuration manager.
 __author__ = 'Michal Čihař'
 __email__ = 'michal@cihar.com'
 __license__ = '''
-Copyright © 2007 - 2008 Michal Čihař
+Copyright © 2007 - 2009 Michal Čihař
 
 This program is free software; you can redistribute it and/or modify it
 under the terms of the GNU General Public License version 2 as published by

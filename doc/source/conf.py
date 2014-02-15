8cd0c7263c354766c5c2699e79a4508621f2bd00
Andrew Shadura <andrew@shadura.me>
2014-02-15 22:59:34 +0100

    Version 0.12
    --HG--
    extra : amend_source : 5842d5b354182026df0aa25af2de00a07e51ae76

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -48,7 +48,7 @@ copyright = u'2007 - 2010, Michal Čihař'
 # built documents.
 #
 # The short X.Y version.
-version = '0.11'
+version = '0.12'
 # The full version, including alpha/beta/rc tags.
 release = version
 
